c53a0b1baed1eaa0923e2af18f35c55d3f322867
Kevin Krato <kratok08@gmail.com>
2020-01-26 16:22:56 -0700

    Add profile route to app

--- server.js
+++ server.js
@@ -15,5 +15,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 app.listen(PORT, () => console.log(`Connected to port:${PORT}`));
